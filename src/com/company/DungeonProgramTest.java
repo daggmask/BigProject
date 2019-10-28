c709cdce8d723976438472e25aa1c3b3eb2a1b43
Martin Hellström <martver12@gmail.com>
2019-10-28 09:53:53 +0100

    Fixed jUnit

--- src/com/company/DungeonProgramTest.java
+++ src/com/company/DungeonProgramTest.java
@@ -1,10 +1,14 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DungeonProgramTest {
+    ArrayList<Monsters> monsters = new ArrayList<>();
+    ArrayList<LootOrTreasures.Treasure> lootOrTreasures = new ArrayList<>();
+    Random rand = new Random();
 
     @org.junit.jupiter.api.Test
     void showLoot() {
@@ -35,9 +39,26 @@ class DungeonProgramTest {
 
     @org.junit.jupiter.api.Test
     void addFeralMonsters() {
+        int i = 0;
+        while (i < 5) {
+            MonsterFactory.MonsterAffix monsterAffix = MonsterFactory.MonsterAffix.values()[rand.nextInt(MonsterFactory.MonsterAffix.values().length)];
+            MonsterFactory.MonsterType monsterType = MonsterFactory.MonsterType.values()[rand.nextInt(MonsterFactory.MonsterType.values().length)];
+            monsters.add(new Monsters(monsterAffix.monsterAffix, monsterType.monsterType, rand.nextInt((10 - 1) + 1) + 1));
+            monsters.get(i).addEquipment("No gear due to feral monster");
+            monsters.get(i).setTitle("Feral ");
+            i++;
+            System.out.println(monsters.size());
+        }
     }
 
     @org.junit.jupiter.api.Test
     void addPresetLootOrTreasure() {
+        int presetLootOrTreasureAmount = 0;
+        while (presetLootOrTreasureAmount < 3){
+            LootOrTreasures.Treasure loot = LootOrTreasures.Treasure.values()[rand.nextInt(LootOrTreasures.Treasure.values().length)];
+            lootOrTreasures.add(loot);
+            System.out.println(loot.lootAndTreasure + " has been found and added to treasury");
+            presetLootOrTreasureAmount++;
+        }
     }
 }
