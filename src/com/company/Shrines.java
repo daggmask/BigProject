c3afef09e08ba9228cf8d7a07bfd615a03d8eb77
Martin Hellström <martver12@gmail.com>
2019-10-28 13:00:16 +0100

    Added enum description for shrine and traps

--- src/com/company/Shrines.java
+++ src/com/company/Shrines.java
@@ -6,10 +6,16 @@ package com.company;
 
 public enum Shrines {
     /**
-     * Description used for print upon shrine encountered
+     * Healing shrine, heals hero
      */
     HEALING("The hero encounters a healing shrine and gets blessed by a surge of light"),
+    /**
+     * Curse shrine, lowers hero damage output
+     */
     CURSE("The hero encounters a cursed shrine and feels dread looming"),
+    /**
+     * Damage shrine, increases hero damage output
+     */
     DAMAGE("The hero encounters a damage shrine feels stronger by the presence of a god of war");
 
     public String shrineDescription;
--- src/com/company/Traps.java
+++ src/com/company/Traps.java
@@ -2,18 +2,37 @@ package com.company;
 
 /**
  * <h1>Different traps with event description</h1>
+ * Traps deal percentage damage to hero
  */
 
 public enum Traps {
     /**
-     * Trap description used to print event upon trap encounter
+     * Boulder trap, rolls and damages hero
      */
     BOULDER("The hero gets hit by a rolling boulder"),
+    /**
+     * Plate trap that opens floor beneath hero and hero falls into spikes
+     */
     SPIKE_TRAP("The hero steps on a brick that triggers spikes to emerge from the ground"),
+    /**
+     * Swinging axe from roof
+     */
     SWING_TRAP("The hero walks across a bridge in the dungeon and suddenly a large axe swings from the roof"),
+    /**
+     * Shoots arrows from wall
+     */
     ARROW_TRAP("As the hero walks through a corridor, an arrow shoots at the hero"),
+    /**
+     * Sprouts fire from different angles
+     */
     FIRE_TRAP("Fire sprouts from the floor and roof"),
+    /**
+     * Magic circle that damages hero
+     */
     MAGIC_TRAP("A magic circle shows up beneath the hero's feet and starts glowing"),
+    /**
+     * Opens floor and hero falls great height
+     */
     FALL_TRAP("The floor opens beneath the hero and the hero falls into the pit revealed");
 
     public String trapDescription;
